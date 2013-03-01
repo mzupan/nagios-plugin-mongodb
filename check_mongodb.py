6ef9f9cbb15a35791be1281d5a789f0ea5b76865
martin.buechler <martin.buechler@ishisystems.com>
2013-03-01 12:21:22 -0500

    added row_count check

--- check_mongodb.py
+++ check_mongodb.py
@@ -122,7 +122,7 @@ def main(argv):
                  choices=['connect', 'connections', 'replication_lag', 'replication_lag_percent', 'replset_state', 'memory', 'memory_mapped', 'lock', 
                           'flushing', 'last_flush_time', 'index_miss_ratio', 'databases', 'collections', 'database_size','queues','oplog','journal_commits_in_wl',
                           'write_data_files','journaled','opcounters','current_lock','replica_primary','page_faults','asserts', 'queries_per_second',
-                          'page_faults', 'chunks_balance', 'connect_primary', 'collection_state'])
+                          'page_faults', 'chunks_balance', 'connect_primary', 'collection_state', 'row_count'])
     p.add_option('--max-lag',action='store_true',dest='max_lag',default=False,help='Get max replication lag (for replication_lag action only)')
     p.add_option('--mapped-memory',action='store_true',dest='mapped_memory',default=False,help='Get mapped memory instead of resident (if resident memory can not be read)')
     p.add_option('-D', '--perf-data', action='store_true', dest='perf_data', default=False, help='Enable output of Nagios performance data')
@@ -228,6 +228,8 @@ def main(argv):
         return check_connect_primary(con, warning, critical, perf_data)
     elif action == "collection_state":
         return check_collection_state(con, database, collection)
+    elif action == "row_count":
+        return check_row_count(con, database, collection, warning, critical, perf_data)
     else:
         return check_connect(host, port, warning, critical, perf_data, user, passwd, conn_time)
 
@@ -1175,6 +1177,16 @@ def check_collection_state(con, database, collection):
     except Exception, e:
         return exit_with_general_critical(e)
 
+def check_row_count(con, database, collection, warning, critical, perf_data):
+    try:
+        count = con[database][collection].count()
+        message = "Row count: %i" % (count)
+        message += performance_data(perf_data,[(count,"row_count",warning,critical)])
+
+        return check_levels(count,warning,critical,message)
+
+    except Exception, e:
+        return exit_with_general_critical(e)
 
 def build_file_name(host, action):
     #done this way so it will work when run independently and from shell
